23b7b195aef0786bf7ca475db2b21a9bddeea8d7
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-10-24 20:02:53 -0300

    feat: add LoginValidationFactory

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
 describe('SignUp Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
